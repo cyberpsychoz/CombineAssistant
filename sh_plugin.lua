b8546f36a6ce68d3157bdf8e00afb3b9a283fda3
Крыжовник <vasyl.lebedev@yandex.ru>
2023-02-24 23:39:34 +0300

    Update sh_plugin.lua

--- sh_plugin.lua
+++ sh_plugin.lua
@@ -1,7 +1,7 @@
 
 PLUGIN.name = "Combine Assistant"
 PLUGIN.author = "Aspect™"
-PLUGIN.description = "Adds a set of useful context shortcuts for Combine Units."
+PLUGIN.description = "Добавляет набор полезных контекстных ярлыков для комбинированных устройств."
 
 ix.util.Include("cl_hooks.lua")
 ix.util.Include("cl_plugin.lua")
